8dc0fc4f4f0db6ac9675b6c330f6ffe5ce082764
Nicolas Roman <nick.26blue@gmail.com>
2021-05-24 17:34:10 -0300

    gitignore agregado, primeros cambios punto 1

---
+++ TP5/TP5/Program.cs
@@ -0,0 +1,20 @@
+﻿/*using System;
+
+namespace TP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese el numero a invertir: ");
+            numero = Console.Read();
+
+            while (numero <= 0)
+            {
+                Console.WriteLine("Por favor ingrese un numero valido para invertir: ");
+            }
+        }
+    }
+}*/
---
+++ TP5/TP5/Punto1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP5
+{
+    class Punto1
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            Console.WriteLine("Ingrese numero a invertir: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
