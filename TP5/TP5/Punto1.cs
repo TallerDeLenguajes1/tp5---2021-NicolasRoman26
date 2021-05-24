197174bfab0066167833696b5f55ba17dfdc6a25
Nicolas Roman <nick.26blue@gmail.com>
2021-05-24 18:24:37 -0300

    Punto 1 terminado

--- TP5/TP5/Punto1.cs
+++ TP5/TP5/Punto1.cs
@@ -8,11 +8,25 @@ namespace TP5
     {
         static void Main(string[] args)
         {
-            int numero = 0;
+            int numero, aux, inverso=0;
             Console.WriteLine("Ingrese numero a invertir: ");
             numero = Convert.ToInt32(Console.ReadLine());
 
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    aux = numero % 10;
+                    numero = numero / 10;
+                    inverso = inverso * 10 + aux;
+                }
 
+                Console.WriteLine("El numero ingresado al invertirlo queda como: " + inverso);
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es cero o menor que cero, no se invertira.");
+            }
         }
     }
 }
